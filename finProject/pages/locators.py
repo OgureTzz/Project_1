27e70831899218255db597acc3756a009eef9ddc
OgureTzz <ogur9514@yandex.ru>
2022-06-01 14:21:20 +0300

    Добавили локаторы на формы Логина и Регистрации

--- finProject/pages/locators.py
+++ finProject/pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.XPATH, '//a[@id="login_link"]')
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.XPATH, '//form[@id="login_form"]')
+    REG_Form = (By.XPATH, '//form[@id="register_form"]')
